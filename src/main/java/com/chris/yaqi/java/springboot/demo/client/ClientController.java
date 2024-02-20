979b2ae88f3ef5fc307645f7ee1904e5514e6e24
Chris <chrisyaqili@gmail.com>
2024-02-20 12:14:11 -0800

    Add PUT DELETE

--- src/main/java/com/chris/yaqi/java/springboot/demo/client/ClientController.java
+++ src/main/java/com/chris/yaqi/java/springboot/demo/client/ClientController.java
@@ -1,9 +1,7 @@
 package com.chris.yaqi.java.springboot.demo.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -21,6 +19,22 @@ public class ClientController {
     public List<Client> getClient(){
         return clientService.getClient();
     }
+    @PostMapping
+    public void registerNewClient(@RequestBody Client client){
+        clientService.addNewClient(client);
+    }
+    @DeleteMapping(path = "{clientID}")
+    public void deleteClient(@PathVariable("clientID") Long clientId){
+        clientService.deleteClient(clientId);
+    }
+    @PutMapping(path = "{clientId}")
+    public void updateClient(
+            @PathVariable("clientId") Long clientId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email){
+        clientService.updateClient(clientId,name,email);
+    }
+
 }
 
 
--- src/main/java/com/chris/yaqi/java/springboot/demo/client/ClientService.java
+++ src/main/java/com/chris/yaqi/java/springboot/demo/client/ClientService.java
@@ -3,11 +3,14 @@ package com.chris.yaqi.java.springboot.demo.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -20,5 +23,40 @@ public class ClientService {
 
     public List<Client> getClient(){
         return clientRepository.findAll();
+    }
+
+    public void addNewClient(Client client) {
+        Optional<Client> clientByEmail= clientRepository.findClientByEmail(client.getEmail());
+        if(clientByEmail.isPresent()){
+            throw new IllegalStateException("Email has been taken.");
+        }else {
+            clientRepository.save(client);
+        }
+
+
+    }
+
+    public void deleteClient(Long id) {
+        if(!clientRepository.existsById(id)){
+            throw new IllegalStateException("Client with id " + id + " not found.");
+        }else{
+            clientRepository.deleteById(id);
+        }
+    }
+    @Transactional
+    public void updateClient(Long clientId, String name, String email) {
+
+        Client client = clientRepository.findById(clientId).orElseThrow(
+                () -> new IllegalStateException("Client with id " + clientId + " not found."));
+        if(name != null && !name.isEmpty() && !Objects.equals(name,client.getName())){
+            client.setName(name);
+        }
+        if(email != null && !email.isEmpty() && !Objects.equals(email,client.getEmail())){
+            if(clientRepository.findClientByEmail(email).isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            client.setEmail(email);
+        }
+
     }
 }
